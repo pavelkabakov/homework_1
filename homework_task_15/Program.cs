92e5f3111222e42a44b02ca4284ffcf22c702501
Павел Кабаков <pavelkabakov@gmail.com>
2023-03-26 18:51:19 +0300

    Задачи 10, 13, 15 - Закончены

--- homework_task_15/Program.cs
+++ homework_task_15/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите номер дня недели (от 1 до 7)");
+string number = Console.ReadLine();
+if (number == "6" | number == "7")
+{
+    Console.WriteLine("Сегодня выходной");
+}
+else
+{
+    Console.WriteLine("Сегодня рабочий день");
+}
